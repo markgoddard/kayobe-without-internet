f3ee02c1984e99a1ebc88e28d3a37613acce4715
Mark Goddard <mark@stackhpc.com>
2018-06-27 16:24:16 +0000

    update kayobe.sh

--- kayobe.sh
+++ kayobe.sh
@@ -3,6 +3,5 @@
 set -e
 
 git clone http://10.0.0.8:81/github/openstack/kayobe
-cd kayobe
-mkdir -p config/src/kayobe-config
-git clone http://10.0.0.8:81/github/stackhpc/dev-kayobe-config config/src/kayobe-config
+mkdir -p kayobe/config/src
+ln -s $(pwd)/kayobe-config $(pwd)/kayobe/config/src
